f329ae51c29606b7831411f23069337ba79bbaee
Harikarthick <karthickaitech@gmail.com>
2018-12-14 13:03:41 +0530

    First commit

--- Json/File.swift
+++ Json/File.swift
@@ -18,29 +18,29 @@ extension UIViewController
     }
     
     func callMethod(urlName : URL) ->  [String] {
-     var array : [String] = []
-    let task = URLSession.shared.dataTask(with: urlName) { (data, response, error) in
-        guard let dataResponse = data,
-            error == nil else {
-                print(error?.localizedDescription ?? "Response Error")
-                return }
-        do{
-            let jsonResponse = try JSONSerialization.jsonObject(with:
-                dataResponse, options: []) as? [String:Any]
-            let root = RootClass(fromDictionary: jsonResponse!)
-            for i in root.results{
-                print(i.name!)
-                array.append(i.name)
+        var array : [String] = []
+        let task = URLSession.shared.dataTask(with: urlName) { (data, response, error) in
+            guard let dataResponse = data,
+                error == nil else {
+                    print(error?.localizedDescription ?? "Response Error")
+                    return }
+            do{
+                let jsonResponse = try JSONSerialization.jsonObject(with:
+                    dataResponse, options: []) as? [String:Any]
+                let root = RootClass(fromDictionary: jsonResponse!)
+                for i in root.results{
+                    print(i.name!)
+                    array.append(i.name)
+                }
+            } catch let parsingError {
+                print("Error", parsingError)
             }
-        } catch let parsingError {
-            print("Error", parsingError)
         }
-    }
-    task.resume()
+        task.resume()
         
         return array
     }
-
+    
     
 }
 extension UIAlertController{
